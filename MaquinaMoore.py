e3b6e027925aa2ee4b20b99bce123f81f878bbd9
Maurício Okuyama <mauricio.okuyama@ufv.br>
2023-07-05 07:23:29 -0300

    movido maquinas

--- MaquinaMoore.py
+++ MaquinaMoore.py
@@ -1,8 +1,17 @@
 import random
 import enum
 import itertools
-from estado import Estado
 from saida import Saida
+
+class Estado:
+    def __init__(self, nome, saida, transicoes_dict):
+        self.nome = nome
+        self.saida = saida
+        self.transicoes_dict = transicoes_dict
+
+    def __str__(self):
+        return f"Nome: {self.nome}, Saida: {self.saida}, Transicoes: {self.transicoes_dict}"
+
 
 class MaquinaMoore:
     def __init__(self, estados, estados_inicial, transicoes):
--- estado.py
+++
@@ -1,9 +0,0 @@
-class Estado:
-    def __init__(self, nome, saida, transicoes_dict):
-        self.nome = nome
-        self.saida = saida
-        self.transicoes_dict = transicoes_dict
-    
-    def __str__(self):
-        return f"Nome: {self.nome}, Saida: {self.saida}, Transicoes: {self.transicoes_dict}"
-
